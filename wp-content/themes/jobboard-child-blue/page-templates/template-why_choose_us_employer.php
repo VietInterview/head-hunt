c03db24f2eb64ce5efd879cd6aa5d8ed14d3c39f
Keyz1022 <ntngoc1022@gmail.com>
2018-07-10 13:44:34 +0700

    fix home

--- wp-content/themes/jobboard-child-blue/page-templates/template-why_choose_us_employer.php
+++ wp-content/themes/jobboard-child-blue/page-templates/template-why_choose_us_employer.php
@@ -107,9 +107,9 @@ get_header(); ?>
 		</div>
 
 		<?php
-				if( jobboard_option( 'enable_testimonial' ) ){
-					get_template_part( 'template-parts/homepage', 'testimonials_alt' );
-				}
+			if( jobboard_option( 'enable_testimonial' ) ){
+				get_template_part( 'template-parts/homepage', 'testimonials_alt' );
+			}
 		?>
 	
 
